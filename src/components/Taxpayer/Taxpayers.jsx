51efccd334e1a3ffba0ee51fe8e5555f7e2a5364
Ifeoluwa Makinde <makindeifeoluwa07@gmail.com>
2026-01-14 07:23:06 +0100

    Update Taxpayers.jsx

--- src/components/Taxpayer/Taxpayers.jsx
+++ src/components/Taxpayer/Taxpayers.jsx
@@ -38,7 +38,7 @@ function TaxpayerComp() {
         </p>
       </div>
 
-      <div className="m-auto w-70 lg:max-w-[70%] px-6 flex justify-center items-center gap-2 lg:gap-4 ">
+      <div className="m-auto w-70 max-w-[70%] px-6 flex justify-center items-center gap-2 lg:gap-4 ">
         {tabs.map((tab) => (
           <Button
             key={tab.title}
